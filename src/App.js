963a5ddfce0d91e20ffc37da51947e64c11508ab
Deepak Rathor <rathordeepak1985@gmail.comm>
2021-10-20 22:02:02 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import About from './components/About';
+import Home from './components/Home';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 function App() {
