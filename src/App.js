b27c172725502e1d95db2807b8ebcde2f7f51c25
Deepak Rathor <rathordeepak1985@gmail.comm>
2021-10-20 09:19:57 +0530

    my first navbar

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return <>
+   <ul className="nav justify-content-end">
+  <li className="nav-item">
+    <a className="nav-link active" aria-current="page" href="/">Active</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href="/">Link</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href="/">Link</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link disabled">Disabled</a>
+  </li>
+</ul>
+  </>
+}
+
+export default App;
