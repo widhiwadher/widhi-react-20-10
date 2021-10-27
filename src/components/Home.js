8f5ed11412718b901309081672cbb5c08df0e0e6
Deepak Rathor <rathordeepak1985@gmail.com>
2021-10-27 11:33:49 +0530

    test

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
-import logo from '../img/logo.jpg'
+import logo from '../img/build.png'
 
 const Home = () => {
-    return (
-        <div className="row">
-        
-          <img src={logo} width="100" height="50%" />
-        
+  return (
+    <div className="container-fluid">
+      <div className="row">
+
+        <img src={logo} width="100" height="50%" />
+
       </div>
-    )
+
+    </div>
+  )
 }
 
-export default Home ;
+export default Home;
