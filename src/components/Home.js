71a698221bd5e1a80814f4a8825bb89f9340d350
Deepak Rathor <rathordeepak1985@gmail.comm>
2021-10-27 22:05:14 +0530

    home page first commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,26 @@ import logo from '../img/build.png'
 const Home = () => {
   return (
     <div className="container-fluid">
-      <div className="row">
 
-        <img src={logo} width="100" height="50%" />
+      <div className='row col-10 mx-auto'>
+          <div className='col-md-5 col-lg-5 pt-5 pt-lg-0 order-2'>
+         
+            <h2 className = "pt-5 ">
+              Build Your Dream Space With <strong>BuildEasy</strong>
+            </h2>
+             <h3 className = "pt-4 ">
+              We are the team of experienced builders for building your customised dream space.
+            </h3>
+          </div>
+
+          <div className='col-md-5 col-lg-5 order-1 order-lg-2'>
+            <img src={logo} width="110%" height="110%" />
+          </div>
+        
 
       </div>
+
+
 
     </div>
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,10 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-         <div className = "container">
+         <div className = "container-fluid">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand decorate"> <Link to = "/">Builders</Link></a>
+          <a className="navbar-brand decorate"> <Link to = "/">BuildEasy</Link></a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
