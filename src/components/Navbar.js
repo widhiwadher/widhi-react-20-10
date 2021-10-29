a3b868dc03345ca30f875002717504a6103b0a22
Deepak Rathor <rathordeepak1985@gmail.com>
2021-10-29 11:07:44 +0530

    login button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,34 +4,40 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 
 const Navbar = () => {
-    return (
-         <div className = "row-fluid">
-        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+  return (
+    <div className="row-fluid">
+      <nav className="navbar navbar-expand-lg navbar-light bg-dark">
         <div className="container-fluid">
-          <a className="navbar-brand decorate"> <Link to = "/">BuildEasy</Link></a>
+          <a className="navbar-brand decorate"> <Link to="/">BuildEasy</Link></a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav mr-auto">
-              
-            <li className="nav-item decorate
-                nav-link"> <Link to = "/">Home</Link>
+            <ul className="navbar-nav ml-auto">
+
+              <li className="nav-item decorate
+                nav-link"> <Link to="/">Home</Link>
               </li>
               <li className="nav-item decorate
-                nav-link"> <Link to = "/about">About</Link>
+                nav-link"> <Link to="/about">About</Link>
               </li>
               <li className="nav-item decorate
-                nav-link"> <Link to = "/profile">Profile</Link>
+                nav-link"> <Link to="/profile">Profile</Link>
               </li>
-             
+              <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+                <button className=' btn btn-primary  me-md-2'>Login</button>
+                <button className='btn btn-success'>SignUp</button>
+              </div>
+
             </ul>
+
+
           </div>
         </div>
       </nav>
-      </div>
-        
-    )
+    </div>
+
+  )
 }
 
 
