3372fbd4a3410b675351d2125609aeefcb439607
Deepak Rathor <rathordeepak1985@gmail.com>
2021-11-03 11:27:36 +0530

    searchbox on progress

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,8 +7,8 @@ const Navbar = () => {
   return (
     <div className="row-fluid">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand decorate"> <Link to="/">BuildEasy</Link></a>
+        <div className="container-fluid ">
+          <a className="navbar-brand decorate"> <Link to="/"><strong>BuildEasy</strong></Link></a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
