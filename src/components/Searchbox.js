2eea89343b82b25d6a9cd4e592b88178b54b7a93
Deepak Rathor <rathordeepak1985@gmail.comm>
2021-11-03 22:01:18 +0530

    search button on

--- src/components/Searchbox.js
+++ src/components/Searchbox.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import Button from 'react-bootstrap/Button'
 const Searchbox = () => {
     return (
         <div>
-            <div class="shadow p-3 mb-5 bg-body rounded">Regular shadow</div>
+            <div class="shadow p-3 mb-5 bg-body rounded">
+            <Button variant="outline-success">Search</Button>{' '}
+            </div>
         </div>
     )
 }
